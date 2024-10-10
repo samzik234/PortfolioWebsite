5c033e7b4dd558e590500a2c29d88e978f04e689
samzik234 <simonazike155@gmail.com>
2024-10-10 09:00:15 +0100

    PortfolioSite

---
+++ main.js
@@ -0,0 +1,8 @@
+// const button = document.querySelector(".title button");
+// button.addEventListener("click");
+// button.style.color="white"
+// button.style.backgroundColor="orange" button.style.borderRadius="10px  20px" button.style.padding="10px"
+//  button.style.fontSiz="16px"
+//  button.style.cursor="pointer"
+//  button.style.transition="all 0.3s ease;"
+//  button="hover backgroundColor: #14213d"

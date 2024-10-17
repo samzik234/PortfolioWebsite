43aa84114670331911fa5996d570e87070fa051f
samzik234 <simonazike155@gmail.com>
2024-10-18 00:47:00 +0100

    mediaQueryandBackround

--- JS/main.js
+++ JS/main.js
@@ -6,3 +6,14 @@
 //  button.style.cursor="pointer"
 //  button.style.transition="all 0.3s ease;"
 //  button="hover backgroundColor: #14213d"
+
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu  = document.querySelector('.mobile-nav');
+menu_btn.addEventListener('click', function() {
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+
+  
+
+});
+

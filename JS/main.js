36aea0b9191a2e871e9b61e4d365320174f6e6ae
samzik234 <simonazike155@gmail.com>
2024-10-21 19:26:51 +0100

    addedResumebutton

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,4 @@
-// const button = document.querySelector(".title button");
-// button.addEventListener("click");
-// button.style.color="white"
-// button.style.backgroundColor="orange" button.style.borderRadius="10px  20px" button.style.padding="10px"
-//  button.style.fontSiz="16px"
-//  button.style.cursor="pointer"
-//  button.style.transition="all 0.3s ease;"
-//  button="hover backgroundColor: #14213d"
-
+ //Mobile menue toggle functionality.
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu  = document.querySelector('.mobile-nav');
 menu_btn.addEventListener('click', function() {
